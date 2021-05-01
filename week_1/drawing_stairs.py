9a3109cb4b2fd38805982a99240f9653f9a43afb
panchenkoanton <panchenkoantonvl@gmail.com>
2021-05-01 21:38:37 +0300

    solved second task

---
+++ week_1/drawing_stairs.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def draw_stairs():
+    """
+    Draws stairs based on given size.
+    """
+    size = int(sys.argv[1])
+    return '\n'.join([' ' * (size-(idx + 1)) + '#' * (idx + 1) for idx in range(size)])
+
+
+if __name__ == '__main__':
+    print(draw_stairs())
